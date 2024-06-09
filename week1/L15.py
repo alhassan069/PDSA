baf5ac3ddb9ca3f6dfc1a5a0ef5fbd24b55fbd57
Alimul Hassan <alhassan069@gmail.com>
2024-06-09 12:57:03 +0530

    added lession 15

---
+++ week1/L15.py
@@ -0,0 +1,34 @@
+# simplifying the gcd 
+# if d divides m and n, then d also divides (m-n)
+# using recursion
+# time complexity O(max(m,n))
+
+def gcd(m,n):
+    (a,b) = (max(m,n), min(m,n))
+    if a%b == 0:
+        return b
+    else:
+        return (gcd(b,a-b))
+    
+# Euclid's algorithm
+# suppose n doesnot divide m
+# then m = qn + r #quotient and reminer
+# suppose d divides both m and n
+# then m = ad, n = bd
+# m = qn + r  -> ad = q(bd) + r
+# r must be in the form of cd
+# 
+# EUCLID'S ALGORITHM
+# if n divides m, gcd(m,n) = n
+# otherwise compute gcd(n,m mod n) 
+
+def gcd_optimized(m,n):
+    (a,b) = (max(m,n), min(m,n))
+    if a%b == 0:
+        return b
+    else:
+        return gcd_optimized(b, a%b)
+
+
+
+
